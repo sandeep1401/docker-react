00a892db80d475eca7268775ef05c90560b96bca
Sandeep <ssingh@kiwiup.com>
2021-09-19 16:48:07 +0530

    Feature branch commit again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to finally work no more fuckups added from feature branch
+          Edit <code>src/App.js</code> and save to finally work no more fuckups added from feature branch again
         </p>
         <a
           className="App-link"
